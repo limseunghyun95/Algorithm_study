250ad6eccf5dcf8eeb5b3529cf67873492b62f60
Limseunghyun95 <limseunghyun95@gmail.com>
2021-07-12 14:11:47 +0000

    [lim] 주석 추가

--- baekjoon/p_2920.py
+++ baekjoon/p_2920.py
@@ -1,9 +1,11 @@
+# 데이터 수신
 data = list(map(int, input().split(" ")))
 
-first = data[0]
+first = data[0] # 초기값만 저장
 
 for index, item in enumerate(data[1:]):
 
+    # 인덱스가 증가함에 따라 이전 데이터와의 차이로 오름차순, 내림차순 여부를 확인
     if item - first == index + 1:
         answer = "ascending"
     elif item - first == -1 * (index + 1):
