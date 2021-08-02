8d8acc1ecdf82e9bd9e1aae336e47b1b83279d29
Limseunghyun95 <limseunghyun95@gmail.com>
2021-08-02 10:05:48 +0000

    [Lims] 프로그래머스 Weekly Challenge 1주차 코드 업로드

---
+++ programmers/weekly_1.py
@@ -0,0 +1,12 @@
+def solution(price, money, count):
+
+    for i in range(1, count + 1):
+        money -= price * i
+    
+    if money > 0:
+        return 0
+    else:
+        return -1 * money
+
+
+solution(3, 20, 4)
