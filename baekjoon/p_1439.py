5cffff4a4ddad3135150076ac9e398d7749718a7
Limseunghyun95 <limseunghyun95@gmail.com>
2021-10-26 07:01:22 +0000

    1439번 백준 온라인 저지 문제 2021.10.26 풀이

--- baekjoon/p_1439.py
+++ baekjoon/p_1439.py
@@ -1,29 +1,15 @@
 # source : https://www.acmicpc.net/problem/1439
 
-S = input()
+s = input()
 
+result = [0, 0]
 
-# 0으로 만들어주는 케이스에서 뒤집는 횟수 계산
-is_continue = False
-zero_cnt = 0
-for idx, value in enumerate(S):
-    if value == "1":
-        if is_continue == False:
-            zero_cnt += 1
-            is_continue = True
+for i in [0, 1]:
+    prev = -2
+    for idx, value in enumerate(s):
+        if int(value) != i:
+            if idx != prev + 1:
+                result[i] += 1
+            prev = idx
 
-    else:
-        is_continue = False
-
-# 1로 만들어주는 케이스에서 뒤집는 횟수 계산
-is_continue = False
-one_cnt = 0
-for idx, value in enumerate(S):
-    if value == "0":
-        if is_continue == False:
-            one_cnt += 1
-            is_continue = True
-    else:
-        is_continue = False
-
-print(min(one_cnt, zero_cnt)) # 최솟값 출력
+print(min(result))
