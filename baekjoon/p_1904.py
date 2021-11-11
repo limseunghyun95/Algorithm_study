ddbd3d3185bf37c5b63c253281412a4cab420480
limseunghyun-git <58515197+Limseunghyun95@users.noreply.github.com>
2021-11-11 10:22:33 +0900

    백준 온라인 저지 1904번 문제 풀이 2021.11.11

---
+++ baekjoon/p_1904.py
@@ -0,0 +1,12 @@
+# source : https://www.acmicpc.net/problem/1904
+
+n = int(input())
+
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+    
+print(dp[n]) 
