c1cad4c4585de53dcaf6de5f94448ed19bb7986e
Limseunghyun95 <limseunghyun95@gmail.com>
2021-11-14 06:23:47 +0000

    백준 온라인 저지 1874번 문제 풀이 2021.11.14

---
+++ baekjoon/p_1874.py
@@ -0,0 +1,24 @@
+# source : https://www.acmicpc.net/problem/1874
+
+n = int(input())
+
+count = 1
+stack = []
+result = []
+
+for _ in range(1, n + 1):
+
+    data = int(input())
+
+    while count <= data:
+        stack.append(count)
+        count += 1
+        result.append("+")
+    if stack[-1] == data:
+        stack.pop()
+        result.append("-")
+    else:
+        print("NO")
+        exit(0)
+
+print("\n".join(result))
