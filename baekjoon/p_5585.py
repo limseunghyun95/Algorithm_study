d8a8a4bda81a2679877b4ad61908ab9991338182
Limseunghyun95 <limseunghyun95@gmail.com>
2021-11-19 09:40:04 +0000

    백준 온라인 저지 5585번 문제 풀이 2021.11.19

--- baekjoon/p_5585.py
+++ baekjoon/p_5585.py
@@ -1,14 +1,14 @@
 # source : https://www.acmicpc.net/problem/5585
 
+n = int(input())
+coin = [500, 100, 50, 10, 5, 1]
+
 answer = 0
-money = int(input())
+n = 1000 - n
 
-chn = 1000 - money # 거스름돈 계산
-
-for i in [500, 100, 50, 10, 5, 1]: # 크기가 큰 잔돈부터 시작하여 갯수 카운트
+for c in coin:
+    cnt = int(n / c)
+    answer += cnt
+    n %= c
     
-    if chn >= i:
-        answer += int(chn / i) 
-        chn = chn % i    
-
 print(answer)
