46cdb650f504aba5ed80dd24d4f9b19b67b55462
Limseunghyun95 <limseunghyun95@gmail.com>
2021-11-22 05:18:03 +0000

    프로그래머스 두 정수 사이의 합 문제 풀이 2021.11.22

---
+++ programmers/p_12912.py
@@ -0,0 +1,19 @@
+# source : https://programmers.co.kr/learn/courses/30/lessons/12912
+
+def solution(a, b):
+    
+    answer = 0
+    
+    if a >= b:
+        big, small = a, b
+    else:
+        big, small = b, a
+        
+    for i in range(small, big + 1):
+        answer += i
+        
+    print(answer)
+        
+    return answer
+
+solution(3,5)
