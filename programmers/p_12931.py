b63950efa89bf23ecb45c5fb79d0258277cf337d
Limseunghyun95 <limseunghyun95@gmail.com>
2021-11-22 06:54:35 +0000

    프로그래머스 자릿수 더하기 문제 풀이 2021.11.22

---
+++ programmers/p_12931.py
@@ -0,0 +1,10 @@
+# source : https://programmers.co.kr/learn/courses/30/lessons/12931
+
+def solution(n):
+    
+    answer = 0
+    
+    for i in str(n):
+        answer += int(i)
+
+    return answer
