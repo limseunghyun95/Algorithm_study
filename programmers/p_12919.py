852c24483888eae2986445e70a713e9de8c1fda7
Limseunghyun95 <limseunghyun95@gmail.com>
2021-11-22 05:43:37 +0000

    프로그래머스 서울에서 김서방 찾기 문제 풀이 2021.11.22

---
+++ programmers/p_12919.py
@@ -0,0 +1,10 @@
+# source : https://programmers.co.kr/learn/courses/30/lessons/12919
+
+def solution(seoul):
+    
+    answer = '김서방은 {}에 있다'.format(str(seoul.index("Kim")))
+    
+    return answer
+
+
+print(solution(["Jane", "Kim"]))
