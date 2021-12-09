2bd72a666cb5d4fdbf114de7b854c0870e07ffd8
Limseunghyun95 <limseunghyun95@gmail.com>
2021-12-09 00:34:13 +0000

    leetcode fibonacci number 문제 풀이 2021.12.09

--- leetcode/fibonacci_number.py
+++ leetcode/fibonacci_number.py
@@ -1,17 +1,15 @@
+class Solution:
+    def fib(self, n):
 
-def fib(n):
-    
-    if n in [0, 1]:
-        return n
-    
-    fibo = [0 for i in range(n+1)]
-    
-    fibo[0] = 0
-    fibo[1] = 1
-    
-    for i in range(2, n+1):
-        fibo[i] = fibo[i-1] + fibo[i-2]
-    
-    return fibo[n]
-
-print(fib(30))
+        if n in [0, 1]:
+            return n
+        
+        fibo = [0 for i in range(n+1)]
+        
+        fibo[0] = 0
+        fibo[1] = 1
+        
+        for i in range(2, n+1):
+            fibo[i] = fibo[i-1] + fibo[i-2]
+        
+        return fibo[n]
