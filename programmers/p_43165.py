d55e330e46f84d3070fc4b6528eeed160d8aa8ef
Limseunghyun95 <limseunghyun95@gmail.com>
2021-12-28 13:14:46 +0000

    ADD p_43165.py
    Author : 임승현
    Description : 프로그래머스 타켓 넘버 문제 풀이

---
+++ programmers/p_43165.py
@@ -0,0 +1,24 @@
+# source : https://programmers.co.kr/learn/courses/30/lessons/43165
+
+from collections import deque
+
+def solution(numbers, target):
+
+    answer = 0
+
+    # 초기화 (합계, 인덱스 번호)
+    queue = deque([(0, 0)]) # 중복을 위해 set 이용
+
+    while queue: # 스택이 비어있을때까지 반복
+        _sum, idx = queue.popleft()
+
+        if _sum == target and idx == len(numbers): # 모두 순회하고, 합계가 target인 경우에만 count up
+            answer += 1
+
+        else:
+            if idx < len(numbers): # numbers 범위에 벗어나지 않은 선에서 deque에 값 추가
+                number = numbers[idx]
+                queue.append((_sum + number, idx + 1))
+                queue.append((_sum - number, idx + 1))
+
+    return answer        
