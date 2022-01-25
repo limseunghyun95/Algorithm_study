6b825bb74bfffe5ec62437bf4a03c2c0fa09c2d5
Limseunghyun95 <limseunghyun95@gmail.com>
2022-01-25 05:59:20 +0000

    MOD p_11399.py
    Author : 임승현
    Description
     - input에서 sys로 입력 방법 변경
     - 입력받는 시간 리스트 정렬시 기본 라이브러리 정렬로 변경

--- baekjoon/p_11399.py
+++ baekjoon/p_11399.py
@@ -1,24 +1,15 @@
 # source : https://www.acmicpc.net/problem/11399
 
-N = int(input())
+import sys
 
-data = list(map(int, input().split(" ")))
-    
-# bubble sort
-for i in range(len(data) - 1):
-    swap = False
-    for j in range(len(data) - i - 1):
-        if data[j] > data[j+1]:
-            temp = data[j+1]
-            data[j+1] = data[j]
-            data[j] = temp
-            swap = True   
-            
-    if swap == False:
-        break
+N = int(sys.stdin.readline().strip())
+P = list(map(int, sys.stdin.readline().strip().split()))
 
 answer = 0
-for i in range(1, len(data)+1):
-    answer += sum(data[:i])
+
+P = sorted(P)
+
+for i in range(N):
+    answer += sum(P[:i+1])
     
 print(answer)
