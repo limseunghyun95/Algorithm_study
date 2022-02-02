10e9d0976fab9276f966d6bf5a8bef421348a569
Limseunghyun95 <limseunghyun95@gmail.com>
2022-02-02 13:43:18 +0000

    MOD p_1439.py
    Author : 임승현
    Description
     - 백준 온라인 저지 뒤집기 2022.02.02 풀이

--- baekjoon/p_1439.py
+++ baekjoon/p_1439.py
@@ -1,15 +1,18 @@
 # source : https://www.acmicpc.net/problem/1439
 
-s = input()
+import sys
+S = sys.stdin.readline().strip()
 
 result = [0, 0]
 
 for i in [0, 1]:
-    prev = -2
-    for idx, value in enumerate(s):
-        if int(value) != i:
-            if idx != prev + 1:
+    is_continue = False
+    for idx, value in enumerate(S):
+        if value != str(i):
+            if is_continue == False:
                 result[i] += 1
-            prev = idx
+                is_continue = True
+        else:
+            is_continue = False   
 
 print(min(result))
