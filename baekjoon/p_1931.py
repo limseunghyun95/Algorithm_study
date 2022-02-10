796b540a3b3dd39eab53cd5fa583eedd5f7ff280
limseunghyun95 <limseunghyun95@gmail.com>
2022-02-10 16:15:17 +0900

    MOD p_1931.py
    Author : 임승현
    Description
     - 실행 로그를 보는 부분 삭제

--- baekjoon/p_1931.py
+++ baekjoon/p_1931.py
@@ -6,20 +6,18 @@ N = int(sys.stdin.readline().strip())
 meets = []
 
 for _ in range(N):
-    meets.append(list(map(int, sys.stdin.readline().strip().split())))
-    
-meets = sorted(meets, key = lambda x : (x[1], x[0])) # 종료하는 시간 기준으로 정렬하고 종료하는 시간이 동일하다면 시작하는 시간이 빠른 순으로 정렬
+    s_t, e_t = map(int, sys.stdin.readline().strip().split())
+    meets.append([s_t, e_t])
 
-st_time = 0
+sorted_meets = sorted(meets, key = lambda x : (x[1], x[0]))
+
+start_time = 0
 answer = 0
-log = [] # 어떤 회의가 포함되어있는지 확인용
 
-for i in meets:
-    
-    if i[0] >= st_time:
+for m in sorted_meets:
+
+    if start_time == 0 or m[0] >= start_time:
         answer += 1
-        log.append(i)
-        st_time = i[1]
-        
-# print(log)
+        start_time = m[1]
+
 print(answer)
