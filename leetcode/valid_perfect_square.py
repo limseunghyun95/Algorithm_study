372558c76de3efc3dc67e7015fac82e45115b556
limseunghyun95 <limseunghyun95@gmail.com>
2022-11-26 16:18:40 +0900

    Leetcode Valid perfect Square 풀이

---
+++ leetcode/valid_perfect_square.py
@@ -0,0 +1,19 @@
+# source: https://leetcode.com/problems/valid-perfect-square/?envType=study-plan&id=binary-search-i
+
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        i = 1
+
+        while i**2 <= num:
+            if i**2 == num:
+                return True
+            i += 1
+
+        return False
+
+
+# for test
+s = Solution()
+a = s.isPerfectSquare(3000)
+print(a)
